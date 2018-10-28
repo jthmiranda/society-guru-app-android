0148df18c08c4e269908b50f44d5b5ccb81f6243
Jonathan Miranda <miranda.jth@gmail.com>
2018-10-28 11:13:42 -0600

    applying ProviderInstaller

--- app/src/main/java/tech/societyguruapp/MainActivity.java
+++ app/src/main/java/tech/societyguruapp/MainActivity.java
@@ -1,10 +1,18 @@
 package tech.societyguruapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.security.ProviderInstaller;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -16,6 +24,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +35,23 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager itemLinearLayout =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(itemLinearLayout);
+
+        upgradeSecurityProvider();
+
+//        try{
+//            ProviderInstaller.installIfNeeded(this);
+//        } catch (GooglePlayServicesRepairableException e) {
+//            Log.d("PlayServicesRepairable", e.getLocalizedMessage());
+//        } catch (GooglePlayServicesNotAvailableException e) {
+//            e.printStackTrace();
+//        }
+
         getData();
+
     }
 
     private void getData() {
-        Call<PostList> postList = GhostAPI.getService().getPostList();
+        Call<PostList> postList = GhostAPI.getService(mContext).getPostList();
         postList.enqueue(new Callback<PostList>() {
             @Override
             public void onResponse(final Call<PostList> call, final Response<PostList> response) {
@@ -43,10 +64,25 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(final Call<PostList> call, final Throwable t) {
                 mRecyclerView.setAdapter(new PostAdapter(MainActivity.this, new ArrayList<Post>()));
                 //Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
-
             }
         });
     }
+
+    private void upgradeSecurityProvider() {
+        ProviderInstaller.installIfNeededAsync(this, new ProviderInstaller.ProviderInstallListener() {
+            @Override
+            public void onProviderInstalled() {
+
+            }
+
+            @Override
+            public void onProviderInstallFailed(int errorCode, Intent recoveryIntent) {
+                GoogleApiAvailability.getInstance().showErrorNotification(mContext, errorCode);
+                //GooglePlayServicesUtil.showErrorNotification(errorCode, MainActivity.this);
+            }
+        });
+    }
+
 
 
 }
